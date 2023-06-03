3a2de890945ad067c51b317a08b6e59f56076ee0
Igon78 <xpiotek2000x@gmail.com>
2023-06-03 13:25:00 +0200

    Basic functions

---
+++ mastermind+/Assets/Scripts/Dragging.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UIElements;
+
+public class Dragging : MonoBehaviour
+{
+    private bool isDragging;
+    private Vector2 resetPosition;
+    private List<GameObject> correctForms = new List<GameObject>();
+    private GameObject correctForm;
+    private float startPosX;
+    private float startPosY;
+
+    private void Start()
+    {
+        resetPosition = this.transform.localPosition;
+    }
+    public void OnMouseDown()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            
+
+            Vector2 mousePos;
+            mousePos = Input.mousePosition;
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+            startPosX = mousePos.x - this.transform.localPosition.x;
+            startPosY = mousePos.y - this.transform.localPosition.y;
+
+            isDragging = true;
+        }
+    }
+
+    public void OnMouseUp()
+    {
+        isDragging = false;
+
+
+        correctForm = GameObject.Find("ActiveRow");
+        for (int i = 0; i < correctForm.transform.childCount/2; i++)
+        {
+
+            if (Mathf.Abs(this.transform.position.x - correctForm.transform.GetChild(i).gameObject.transform.position.x) <= 1 &&
+           Mathf.Abs(this.transform.position.y - correctForm.transform.GetChild(i).gameObject.transform.position.y) <= 1)
+            {
+                this.transform.localPosition = new Vector2(resetPosition.x, resetPosition.y);
+                correctForm.transform.GetChild(i).gameObject.GetComponent<SpriteRenderer>().color = this.GetComponent<SpriteRenderer>().color;
+                break;
+            }
+
+        }
+
+        this.transform.localPosition = new Vector2(resetPosition.x, resetPosition.y);
+
+        //if (Mathf.Abs(this.transform.position.x - correctForm.transform.position.x) <= 2 &&
+        //   Mathf.Abs(this.transform.position.y - correctForm.transform.position.y) <= 2)
+        //{
+        //    //this.transform.position = new Vector2(correctForm.transform.position.x, correctForm.transform.position.y);
+        //    this.transform.localPosition = new Vector2(resetPosition.x, resetPosition.y);
+        //    correctForm.GetComponent<SpriteRenderer>().color = this.GetComponent<SpriteRenderer>().color;
+        //}
+        //else
+        //{
+            
+        //}
+    }
+
+    private void Update()
+    {
+        if (isDragging)
+        {
+            Vector2 mousePos;
+            mousePos = Input.mousePosition;
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+            this.gameObject.transform.localPosition = new Vector2(mousePos.x - startPosX, mousePos.y - startPosY);
+            startPosX = mousePos.x - this.transform.localPosition.x;
+            startPosY = mousePos.y - this.transform.localPosition.y;
+
+            isDragging = true;
+        }
+    }
+}
