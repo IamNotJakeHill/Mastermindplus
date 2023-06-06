ea88277d24c67b11753be1bfee48310007bccd86
IamNotJakeHill <quba5928@gmail.com>
2023-06-06 10:48:19 +0200

    Menu update
    added button highlights most of the functionality and scrolling background

---
+++ mastermind+/Assets/Script/BGScroll.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGScroll : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float ScrollX = 0.5f;
+    public float ScrollY = 0.5f;
+    void Update()
+    {
+        float OffsetX 
+    }
+}
