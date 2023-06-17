1ba50c348ff05fda930f38459bca22a51aff15a9
IamNotJakeHill <quba5928@gmail.com>
2023-06-17 22:32:09 +0200

    co?

--- mastermind+/Assets/Scripts/BGScroll2.cs
+++ mastermind+/Assets/Scripts/BGScroll2.cs
@@ -2,13 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BGScroll : MonoBehaviour
+public class BGScroll2 : MonoBehaviour
 {
     // Start is called before the first frame update
     public float ScrollX = 0.5f;
     public float ScrollY = 0.5f;
     void Update()
     {
-        float OffsetX 
+        float OffsetX;
     }
 }
